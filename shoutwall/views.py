f3d33520437d23f4438e13d2a23c776ea9b795a0
Mathias Meersschaut <mathias.meersschaut@gmail.com>
2026-06-08 20:12:23 +0200

    shoutwall af

--- shoutwall/views.py
+++ shoutwall/views.py
@@ -1,17 +1,21 @@
-import json
-from django.shortcuts import render
-from django.utils.timezone import localtime
+from django.shortcuts import render, redirect
 from .models import ShoutwallMessage
 
-def format_time(dt):
-    if not dt: return ""
-    return localtime(dt).strftime("%H:%M on %d/%m/%Y")
-
 def index(request):
-    data = PauzeContent.get_singleton()
+    if request.method == "POST":
+        poster = request.POST.get("poster", "").strip()
+        content = request.POST.get("content", "").strip()
+        
+        if poster and content:
+            ShoutwallMessage.objects.create(
+                poster=poster[:100], 
+                content=content[:1000]
+            )
+            return redirect('shoutwall:index')
+
+    messages = ShoutwallMessage.objects.all()
+
     return render(request, 'shoutwall.html', {
-        "title": data.title,
-        "subtitle": data.subtitle,
-        "content": data.content,
-        "time_formatted": format_time(data.last_edited)
+        "title": "Shoutwall",
+        "messages": messages,
     })
